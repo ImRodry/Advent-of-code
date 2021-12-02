34f21eee6026de536e6d453080835b9baafedd56
Rodry <38259440+ImRodry@users.noreply.github.com>
2021-12-02 15:57:33 +0000

    Add comments to day 1

--- Day 1/part1.js
+++ Day 1/part1.js
@@ -2,7 +2,11 @@ const input = require("./input.json")
 let increased = 0
 
 input.forEach((n, i) => {
+    // Don't count the first index
     if (i === 0) return
+    // If the current value is greater than the previous value, increase the counter
     if (n > input[i - 1]) increased++
 })
+
+// Answer: increased
 console.log(increased, input.length)
--- Day 1/part2.js
+++ Day 1/part2.js
@@ -2,9 +2,14 @@ const input = require("./input.json")
 let increased = 0
 
 input.forEach((n, i) => {
+    // Don't count the first 3 indexes (can't calculate the sum)
     if ([0, 1, 2].includes(i)) return
+    // Calculate the sum of the last 3 numbers and the 3 numbers before the current one
     const sum = [n, input[i - 1], input[i - 2]].reduce((a, b) => a + b),
         previousSum = [input[i - 1], input[i - 2], input[i - 3]].reduce((a, b) => a + b)
+    // If sum is bigger than previousSum, increase the counter
     if (sum > previousSum) increased++
 })
+
+// Answer: increased
 console.log(increased, input.length)
