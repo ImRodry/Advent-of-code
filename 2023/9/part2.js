3d961f082e75d9f379158f871efc83b3ab559a22
Rodry <38259440+ImRodry@users.noreply.github.com>
2023-12-09 16:05:17 +0000

    feat(2023): day 9
    easiest part 2 ever

---
+++ 2023/9/part2.js
@@ -0,0 +1,32 @@
+const fs = require("node:fs"),
+	/** @type {string[]} */
+	input = fs.readFileSync("input.txt", "utf-8").trim().split("\n")
+
+let predictionSum = 0
+
+for (const line of input) {
+	const nums = line.split(" ").map(Number)
+	let diff = [nums]
+	while (diff.at(-1).length !== diff.at(-1).filter(n => n === 0).length) diff.push(findDifferences(diff.at(-1)))
+	diff.forEach(r => r.reverse())
+	for (const [i, row] of diff.reverse().entries()) {
+		if (i === 0) row.push(0)
+		else row.push(row.at(-1) - diff[i - 1].at(-1))
+	}
+	predictionSum += diff.at(-1).at(-1)
+}
+
+console.log(predictionSum)
+
+/**
+ * Calculates the differences between each number in the array.
+ * @param {number[]} nums
+ * @returns Array of differences between each number. The returning array's length is one less than the input array's length.
+ */
+function findDifferences(nums) {
+	const differences = []
+	for (let i = 1; i < nums.length; i++) {
+		differences.push(nums[i] - nums[i - 1])
+	}
+	return differences
+}
